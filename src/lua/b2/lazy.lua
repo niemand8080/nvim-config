00af27c0bec9d3e009f9455c0a174daaf90aaa6f
niemand8080 <benhamster443@gmail.com>
2025-02-04 21:33:43 +0100

    Forgot to add files...

--- src/lua/b2/lazy.lua
+++ src/lua/b2/lazy.lua
@@ -15,39 +15,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
--- requierd remap in init
-
 -- Setup lazy.nvim
 require("lazy").setup({
-    spec = {
-        {
-            "nvim-telescope/telescope.nvim",
-            tag = "0.1.8",
-            -- or                              , branch = "0.1.x",
-            dependencies = { "nvim-lua/plenary.nvim" }
-        },
-        { "rose-pine/neovim",     name = "this is" },
-        {
-            "nvim-treesitter/nvim-treesitter",
-            build = ":TSUpdate",
-        },
-        { "nvim-lua/plenary.nvim" },
-        { "ThePrimeagen/harpoon" },
-        { "mbbill/undotree" },
-        { 'neovim/nvim-lspconfig' },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/nvim-cmp' },
-        {
-            "williamboman/mason.nvim",
-            build = ":MasonUpdate"
-        },
-        {
-            "williamboman/mason-lspconfig.nvim",
-        },
-        { "tpope/vim-fugitive" },
-        { 'ThePrimeagen/vim-be-good' }
-    },
+    spec = { "b2.lazy" },
+    change_detection = { notify = false },
 })
--- src/lua/b2/remap.lua
+++ src/lua/b2/remap.lua
@@ -1,4 +1,3 @@
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -18,7 +17,26 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
+vim.keymap.set("n", "<leader>y", "\"_y")
+vim.keymap.set("v", "<leader>y", "\"_y")
+
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!<CR>')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
