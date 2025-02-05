d8541328e51ff4dea0f620ba95caf9bc57900372
niemand <benhamster443@gmail.com>
2025-02-05 13:49:18 +0100

    File for auto cmds

--- src/lua/b2/init.lua
+++ src/lua/b2/init.lua
@@ -1,3 +1,4 @@
 require("b2.remap")
 require("b2.init_lazy")
 require("b2.set")
+require("b2.auto_cmd")
--- src/lua/b2/remap.lua
+++ src/lua/b2/remap.lua
@@ -31,10 +31,3 @@ vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-})
