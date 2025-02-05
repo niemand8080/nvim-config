136144399d72cc4ba9abd3f4c9fcc444f824a249
niemand <benhamster443@gmail.com>
2025-02-05 13:57:09 +0100

    Trimmed telescope.lua, remove wrong line...

--- src/lua/b2/lazy/telescope.lua
+++ src/lua/b2/lazy/telescope.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { 
+    dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-fzf-native.nvim",
     },
@@ -18,7 +18,7 @@ return {
                     fuzzy = true,                    -- false will only do exact matching
                     override_generic_sorter = true,  -- override the generic sorter
                     override_file_sorter = true,     -- override the file sorter
-                    case_mode = "smart_case", 
+                    case_mode = "smart_case",
                 }
             },
         })
