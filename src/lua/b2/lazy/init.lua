406772a941167bbb89e60ab2a69bcc5175ba4108
niemand <benhamster443@gmail.com>
2025-02-06 16:16:58 +0100

    Add remote-sshfs

--- src/lua/b2/lazy/init.lua
+++ src/lua/b2/lazy/init.lua
@@ -2,6 +2,18 @@ return {
     -- For telescope
     { "nvim-lua/plenary.nvim" },
     { 'nvim-telescope/telescope-fzf-native.nvim' },
+    {
+        "nosduco/remote-sshfs.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        pin = true,
+        config = function()
+            require("remote-sshfs").setup({
+                log = {
+                    enable = true
+                }
+            })
+        end
+    },
     -- VimBeGood
     { 'ThePrimeagen/vim-be-good' },
     -- For LSP
@@ -10,3 +22,4 @@ return {
     { 'hrsh7th/nvim-cmp' },
     { "williamboman/mason-lspconfig.nvim" },
 }
+
--- src/lua/b2/lazy/telescope.lua
+++ src/lua/b2/lazy/telescope.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-fzf-native.nvim",
+        "nosduco/remote-sshfs.nvim",
     },
     default = {
         file_ignore_patterns = {
@@ -15,17 +16,42 @@ return {
         require('telescope').setup({
             extensions = {
                 fzf = {
-                    fuzzy = true,                    -- false will only do exact matching
-                    override_generic_sorter = true,  -- override the generic sorter
-                    override_file_sorter = true,     -- override the file sorter
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true,    -- override the file sorter
                     case_mode = "smart_case",
                 }
             },
         })
 
         require("telescope").load_extension('fzf')
+        require('telescope').load_extension('remote-sshfs')
 
-        local builtin = require('telescope.builtin')
+        -- Remote SSHFS 
+        local api = require('remote-sshfs.api')
+        vim.keymap.set('n', '<leader>rc', api.connect, {})
+        vim.keymap.set('n', '<leader>rd', api.disconnect, {})
+        vim.keymap.set('n', '<leader>re', api.edit, {})
+
+        local builtin = require("telescope.builtin")
+        local connections = require("remote-sshfs.connections")
+        vim.keymap.set("n", "<leader>ff", function()
+            if connections.is_connected then
+                api.find_files()
+            else
+                print("Not connncted")
+                -- builtin.find_files()
+            end
+        end, {})
+        vim.keymap.set("n", "<leader>fg", function()
+            if connections.is_connected then
+                api.live_grep()
+            else
+                print("Not connncted")
+                -- builtin.live_grep()
+            end
+        end, {})
+
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
